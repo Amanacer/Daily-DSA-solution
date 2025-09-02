880f0b85d7ffd31e014fdbcc1f91f1af17e2aa46
Aman Singh <135251418+Amanacer@users.noreply.github.com>
2025-09-02 14:12:32 +0530

    Add files via upload

---
+++ Optimal point selsction.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        long k = scanner.nextLong();
+        List<Long> y = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            y.add(scanner.nextLong());
+        }
+
+        Collections.sort(y);
+        long d = y.get(y.size() - 1) - y.get(0);
+        long v = 0;
+        long low = 1;
+        long b = 0;
+        long high = d;
+
+        while (low <= high && b == 0) {
+            long mid = (low + high) / 2;
+
+            if (check(y, k, mid)) {
+                if (!check(y, k, mid + 1)) {
+                    v = mid;
+                    b = 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println(v);
+    }
+
+    public static boolean check(List<Long> y, long k, long g) {
+        long p = y.get(0);
+        long c = 0;
+
+        for (int i = 1; i < y.size(); i++) {
+            long diff = Math.abs(p - y.get(i));
+
+            if (diff >= g) {
+                c++;
+                p = y.get(i);
+            }
+        }
+
+        return c + 1 >= k;
+    }
+}
